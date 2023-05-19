2369ff91f82134050b749165e807c5d4a9e33178
Evan Jones （原文軒） <48502664+evanj0@users.noreply.github.com>
2023-05-19 14:56:36 -0700

    conda activate alias

--- fish/functions/conda_activate.fish
+++ fish/functions/conda_activate.fish
@@ -1,3 +1,8 @@
-function conda_activate
-    eval $CONDA_PATH "shell.fish" "hook" $argv | source
+function conda_activate -d 'Alias to activate a conda environment'
+    set -l initialized_flag _fish_conda_initialized
+    if not set -q $initialized_flag
+        eval $CONDA_PATH "shell.fish" "hook" | source
+        set -g $initialized_flag 1
+    end
+    conda activate $argv
 end
