86ea3863ed1aa7f2f3391b897272a2f809fc4ed3
Evan Jones （原文軒） <48502664+evanj0@users.noreply.github.com>
2023-08-07 18:10:56 +0800

    add yarn and ruby

--- fish/conf.d/00_profile.fish
+++ fish/conf.d/00_profile.fish
@@ -5,3 +5,11 @@ end
 if test -e $HOME/.cargo/bin
     fish_add_path $HOME/.cargo/bin
 end
+
+if test -e $HOME/.yarn/bin
+    fish_add_path $HOME/.yarn/bin
+end
+
+if test -e $HOME/.local/share/gem/ruby/3.0.0/bin
+    fish_add_path $HOME/.local/share/gem/ruby/3.0.0/bin
+end
