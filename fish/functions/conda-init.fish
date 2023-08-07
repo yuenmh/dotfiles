6a6a9687e99122acdafda56901e2c07a4281f3c8
Evan Jones （原文軒） <48502664+evanj0@users.noreply.github.com>
2023-08-07 18:25:37 +0800

    rename conda_init to conda-init

--- fish/functions/conda-init.fish
+++ fish/functions/conda-init.fish
@@ -1,5 +1,4 @@
-function conda_init -d 'Alias to activate a conda environment'
-    # Initialize conda
+function conda-init -d 'Alias to activate a conda environment'
     eval $CONDA_PATH "shell.fish" "hook" | source
     # Disable the conda prompt change because I am using starship
     $CONDA_PATH config --set changeps1 False
