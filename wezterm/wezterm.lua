5b2924d849e6f0d5841166529fdb6458b94522f8
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-05-10 18:23:43 -0700

    add more tabs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -17,5 +17,13 @@ return {
 
 		{ mods = "LEADER", key = "1", action = wezterm.action.ActivateTab(0) },
 		{ mods = "LEADER", key = "2", action = wezterm.action.ActivateTab(1) },
+		{ mods = "LEADER", key = "3", action = wezterm.action.ActivateTab(2) },
+		{ mods = "LEADER", key = "4", action = wezterm.action.ActivateTab(3) },
+		{ mods = "LEADER", key = "5", action = wezterm.action.ActivateTab(4) },
+		{ mods = "LEADER", key = "6", action = wezterm.action.ActivateTab(5) },
+		{ mods = "LEADER", key = "7", action = wezterm.action.ActivateTab(6) },
+		{ mods = "LEADER", key = "8", action = wezterm.action.ActivateTab(7) },
+		{ mods = "LEADER", key = "9", action = wezterm.action.ActivateTab(8) },
+		{ mods = "LEADER", key = "0", action = wezterm.action.ActivateTab(9) },
 	},
 }
